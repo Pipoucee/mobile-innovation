3ddd8821d8396449f928c6c0305ec418776f20ec
mostefai <tarik.mostefai.Ext@inetum.Com>
2024-02-28 12:05:20 +0100

    init project swift

---
+++ MyTech/MyTech/MyTechApp.swift
@@ -0,0 +1,32 @@
+//
+//  MyTechApp.swift
+//  MyTech
+//
+//  Created by MOSTEFAI Tarik on 28/02/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MyTechApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
